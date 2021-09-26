1ba8c9dc2258eb1f23a3efa19a5c1016a9bc233b
Mathis Bebooz <45772124+bebert11@users.noreply.github.com>
2021-09-26 13:02:21 +0000

    changes2

--- music.py
+++ music.py
@@ -21,7 +21,7 @@ class music(commands.Cog):
     await ctx.voice_client.disconnect()
 
   @commands.command()
-  async def play(self,ctx,url,message):
+  async def play(self,ctx,url):
     ctx.voice_client.stop()
     FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
     YDL_OPTIONS = {'format':"bestaudio"}
